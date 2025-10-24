55bc90a4f775bc283e7667551df46ce6b3f0c973
i-am-vengatesh <vengateshbabuvijayakannan@gmail.com>
2025-10-24 20:33:22 +0530

    Create test_app.py

---
+++ backend/tests/test_app.py
@@ -0,0 +1,17 @@
+# backend/tests/test_app.py
+
+from app import add_task, remove_task, get_tasks
+
+def test_add_task():
+    tasks = []
+    add_task(tasks, "Write Jenkinsfile")
+    assert tasks == ["Write Jenkinsfile"]
+
+def test_remove_task():
+    tasks = ["Write Jenkinsfile"]
+    remove_task(tasks, "Write Jenkinsfile")
+    assert tasks == []
+
+def test_get_tasks():
+    tasks = ["Task 1", "Task 2"]
+    assert get_tasks(tasks) == ["Task 1", "Task 2"]
